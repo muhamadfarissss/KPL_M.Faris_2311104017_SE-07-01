71bcbf673ee0c08b29272597157cdafb98714fae
muhamad faris <m.faris5670@gmail.com>
2025-03-13 20:34:04 +0700

    menambah kode buah

---
+++ 04_Oop/jurnal/kodeBuah.js
@@ -0,0 +1,27 @@
+class KodeBuah {
+    constructor() {
+        this.kodeBuah = {
+            "Apel": "A00",
+            "Aprikot": "B00",
+            "Alpukat": "C00",
+            "Pisang": "D00",
+            "Paprika": "E00",
+            "Blackberry": "F00",
+            "Ceri": "H00",
+            "Kelapa": "I00",
+            "Jagung": "J00",
+            "Kurma": "K00",
+            "Durian": "L00",
+            "Anggur": "M00",
+            "Melon": "N00",
+            "Semangka": "O00"
+        };
+    }
+    getKodeBuah(namaBuah) {
+        return this.kodeBuah[namaBuah] || "code tidak ada";
+    }
+}
+
+const kodeBuah = new KodeBuah();
+console.log(kodeBuah.getKodeBuah("Pisang"));
+console.log(kodeBuah.getKodeBuah("Ceri")); 
